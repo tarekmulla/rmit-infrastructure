0876a08356153529b6f94d58a3075179d39e2fb5
Tarek Mulla <tarek@mulla.au>
2023-03-05 15:26:22 +1100

    Fix terraform format

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     Company = var.company
   }, var.tags)
 
-  vpc_cidr = "172.16.0.0/16"
-  public_subnets = flatten([for i in range(0, length(var.apps)): [ for j in range(0, 3): "172.16.${i}.${j*32}/27"]])
-  private_subnets = flatten([for i in range(0, length(var.apps)): [ for j in range(0, 3): "172.16.${i}.${128+(j*32)}/27"]])
+  vpc_cidr        = "172.16.0.0/16"
+  public_subnets  = flatten([for i in range(0, length(var.apps)) : [for j in range(0, 3) : "172.16.${i}.${j * 32}/27"]])
+  private_subnets = flatten([for i in range(0, length(var.apps)) : [for j in range(0, 3) : "172.16.${i}.${128 + (j * 32)}/27"]])
 }
